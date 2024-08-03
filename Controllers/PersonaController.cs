35f915cbd4ff2ffdc9696537da5d5e2cfa0d7e55
Alex Rodriguez <alexrodriguezbenitez@gmail.com>
2024-08-03 16:41:23 -0600

    Creando persona

---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Webappi1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        [HttpPost]
+        [Route("guardar")]
+        public ActionResult<object> guardar([FromBody] Reposo.Persona Persona)
+        {
+            string respuesta = Persona.GuardarPersona(Persona);
+            if (respuesta == "")
+            {
+                return Ok(new { cod_error = 0, msg = "Vehículo guardado satisfactoriamente" });
+            }
+            else
+            {
+                return BadRequest(new { cod_error = -1000, msg = respuesta }); ;
+            }
+
+        }
+        [HttpGet]
+        [Route("listar")]
+        public ActionResult<object> listar()
+        {
+            Reposo.Persona Personas = new Reposo.Persona();
+            return Ok(Personas.ListarPersonas());
+        }
+
+    }
+}
---
+++ Data/Persona.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Webappi1.Reposo
+{
+    public class Persona
+    {
+        private string connectionString = "Server=svr-sql-ctezo.southcentralus.cloudapp.azure.com;Database=db_banco;User Id=usr_admin;Password=usrGuastaUMG!ng;TrustServerCertificate=True;";
+
+        public string cui { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }   
+        public string telefono { get; set; }
+        public string direccion { get; set; }
+
+        public string GuardarPersona(Persona Persona)
+        {
+            string qry = @"
+                INSERT INTO vehiculos (cui, nombre, apellido, telefono, direccion)
+                VALUES (@cui, @nombre, @apellido, @telefono, @direccion)";
+
+            try
+            {
+                //importante, cargar conector SQL
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    //abrir conexion
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(qry, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@cui", Persona.cui);
+                        cmd.Parameters.AddWithValue("@nombre", Persona.nombre);
+                        cmd.Parameters.AddWithValue("@apellido", Persona.apellido);
+                        cmd.Parameters.AddWithValue("@anio", Persona.telefono);
+                        cmd.Parameters.AddWithValue("@direccion", Persona.direccion);
+
+                        // Execute the command
+                        cmd.ExecuteNonQuery();
+                    }
+                    return "";
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exception
+                return (ex.Message);
+            }
+        }
+        public List<Persona> ListarPersonas()
+        {
+            List<Persona> lista = new List<Persona>();
+            string query = "SELECT * FROM personas";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Persona persona = new Persona();
+                                persona.cui = reader["cui"].ToString();
+                                persona.nombre = reader["nombre"].ToString();
+                                persona.apellido = reader["apellido"].ToString();
+                                persona.telefono = reader["anio"].ToString();
+                                persona.direccion = reader["direccion"].ToString();
+                                lista.Add(persona);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return lista;
+        }
+
+    }
+}
